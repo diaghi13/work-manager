12fbd8c069974a93f4c7cfb4cb55e1794341f2c4
Davide Donghi <davide.d.donghi@gmail.com>
2025-04-13 01:59:16 +0200

    Add update worksites status schedules

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,32 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+\Illuminate\Support\Facades\Schedule::call(function () {
+    $worksites = \App\Models\Worksite::query()
+        ->where('status', '<>', \App\Models\Enums\WorksiteStatusEnum::ACCEPTED)
+        ->orWhere('status', '<>', \App\Models\Enums\WorksiteStatusEnum::IN_PROGRESS)
+        ->orWhere('status', '<>', \App\Models\Enums\WorksiteStatusEnum::ACTIVE)
+        //->where('end_date', '<=', now()->toDateString())
+        ->get();
+
+    foreach ($worksites as $worksite) {
+        switch ($worksite->status) {
+            case \App\Models\Enums\WorksiteStatusEnum::ACCEPTED:
+                if (now()->betweenIncluded($worksite->start_date, $worksite->end_date)) {
+                    $worksite->update(['status' => \App\Models\Enums\WorksiteStatusEnum::IN_PROGRESS]);
+                }
+                break;
+
+            case \App\Models\Enums\WorksiteStatusEnum::IN_PROGRESS
+                || \App\Models\Enums\WorksiteStatusEnum::ACTIVE:
+                if (now()->greaterThan($worksite->end_date)) {
+                    $worksite->update(['status' => \App\Models\Enums\WorksiteStatusEnum::COMPLETED]);
+                }
+                break;
+        }
+    }
+})
+    ->daily()
+    ->timezone('Europe/Rome')
+    ->name('update worksite status');
