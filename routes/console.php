565937df7c6405a762f9328070894a24739f7fa1
Davide Donghi <davide.d.donghi@gmail.com>
2025-04-13 19:33:08 +0200

    Fix CRON worksite status update (test every minute)

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,8 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-\Illuminate\Support\Facades\Schedule::call(function () {
+function updateWorksiteStatus(mixed $tenant)
+{
     $worksites = \App\Models\Worksite::query()
         ->where('status', '<>', \App\Models\Enums\WorksiteStatusEnum::ACCEPTED)
         ->orWhere('status', '<>', \App\Models\Enums\WorksiteStatusEnum::IN_PROGRESS)
@@ -31,7 +32,39 @@ Artisan::command('inspire', function () {
                 break;
         }
     }
+}
+
+\Illuminate\Support\Facades\Schedule::call(function () {
+    \Illuminate\Support\Facades\Log::info('Updating worksite status...');
+
+    $tenants = \App\Models\Tenant::query()
+        ->select('id')
+        ->get();
+
+    foreach ($tenants as $tenant) {
+        \App\Models\Tenant::switch(false, $tenant->id);
+
+        \Illuminate\Support\Facades\Log::info('Connected to tenants: ' . $tenant->id);
+
+        try {
+            \Illuminate\Support\Facades\DB::connection('dynamic')->getPdo();
+        } catch (\Exception $e) {
+            \Illuminate\Support\Facades\Log::error('Error connecting to tenant: ' . $tenant->id . ' - ' . $e->getMessage());
+            continue;
+        }
+
+        \Illuminate\Support\Facades\DB::transaction(function () use ($tenant) {
+            \Illuminate\Support\Facades\Log::info('Transaction started for tenant: ' . $tenant->id);
+
+            // Update the worksite status
+            updateWorksiteStatus($tenant);
+        });
+
+        \Illuminate\Support\Facades\Log::info('Worksite status updated for tenant: ' . $tenant->id);
+    }
+
+    \Illuminate\Support\Facades\Log::info('Worksite status update completed.');
 })
-    ->daily()
+    ->everyMinute()
     ->timezone('Europe/Rome')
     ->name('update worksite status');
