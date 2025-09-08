c54e0ef0161e72bc2e35ed9514eb2d1ae8d2ddeb
Davide Donghi <davide.d.donghi@gmail.com>
2025-09-09 01:39:15 +0200

    Change calendar data source

--- app/Livewire/CalendarWidget.php
+++ app/Livewire/CalendarWidget.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\WorkDay;
+use App\Models\WorkDayTime;
 use Filament\Widgets\Widget;
 use Saade\FilamentFullCalendar\Widgets\FullCalendarWidget;
 
@@ -10,15 +11,17 @@ class CalendarWidget extends FullCalendarWidget
 {
     public function fetchEvents(array $info): array
     {
-        return WorkDay::with(['worksite'])
+        return WorkDayTime::with(['work_day' => ['worksite']])
             ->get()
-            ->map(function (WorkDay $workDay) {
-                return [
-                    'id' => $workDay->id,
-                    'title' => $workDay->worksite->name,
-                    'start' => $workDay->date,
-                    'allDay' => true,
-                ];
-            })->all();
+            ->map(function (WorkDayTime $workDayTime) {
+            return [
+                'id' => $workDayTime->id,
+                'title' => $workDayTime->work_day->worksite ? $workDayTime->work_day->worksite->name : 'No Worksite',
+                'start' => $workDayTime->start_datetime->toIso8601String(),
+                'end' => $workDayTime->end_datetime->toIso8601String(),
+                'allDay' => false,
+                'color' => $workDayTime->work_day->type === 'Regular' ? 'blue' : ($workDayTime->work_day->type === 'Overtime' ? 'red' : 'green'),
+            ];
+        })->all();
     }
 }
